aa539d7117a4220540a92256569c025869d8bb6a
domvoetter <domvoetter@gmail.com>
2022-09-28 12:56:01 +0200

    StundenWH-Version

--- lottoziehung.py
+++ lottoziehung.py
@@ -3,19 +3,27 @@ import random
 dict = {}
 
 zahlen = []
-for i in range(1, 46):
-    zahlen.append(i)
-    dict[i] = 0
 
-for i in range(0, 1000):
-    x = 0
-    while x < 6:
-        #rand = int(random.random() * (len(zahlen)-1-x) +1)
-        rand = random.randrange(len(zahlen))
-        zahlen[rand] = zahlen[len(zahlen)-1-x]
-        zahlen[len(zahlen)-1-x] = rand
-        dict[rand+1] += 1
-        x += 1
+def ziehung_range(min, max):
+    for i in range(min, max+1):
+        zahlen.append(i)
+        dict[i] = 0
 
-#print(zahlen)
-print(dict)
+def ziehung(anz):
+    for i in range(anz):
+        index = random.randrange(len(zahlen))
+        value = zahlen[index]
+        zahlen[index] = zahlen[len(zahlen)-1-i]
+        zahlen[len(zahlen)-1-i] = value
+        statistik(index)
+        i += 1
+
+def statistik(x):
+    dict[x+1] +=1
+
+if __name__ == "__main__":
+    ziehung_range(1, 10)
+    for i in range(0, 1000):
+        #print(zahlen)
+        ziehung(6)
+    print(dict)
